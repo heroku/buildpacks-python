d93a271a17c6c8f784eac6dfeaa6db2c0495847b
Ed Morley <501702+edmorley@users.noreply.github.com>
2025-10-15 11:18:43 +0100

    Add support for Python 3.13.9 (#444)
    Release announcement:
    https://blog.python.org/2025/10/python-3139-is-now-available.html
    
    Changelog:
    https://docs.python.org/release/3.13.9/whatsnew/changelog.html#python-3-13-9-final
    
    Binary builds:
    https://github.com/heroku/heroku-buildpack-python/actions/runs/18524770867
    
    GUS-W-19897903.

--- src/python_version.rs
+++ src/python_version.rs
@@ -25,7 +25,7 @@ pub(crate) const LATEST_PYTHON_3_9: PythonVersion = PythonVersion::new(3, 9, 24)
 pub(crate) const LATEST_PYTHON_3_10: PythonVersion = PythonVersion::new(3, 10, 19);
 pub(crate) const LATEST_PYTHON_3_11: PythonVersion = PythonVersion::new(3, 11, 14);
 pub(crate) const LATEST_PYTHON_3_12: PythonVersion = PythonVersion::new(3, 12, 12);
-pub(crate) const LATEST_PYTHON_3_13: PythonVersion = PythonVersion::new(3, 13, 8);
+pub(crate) const LATEST_PYTHON_3_13: PythonVersion = PythonVersion::new(3, 13, 9);
 pub(crate) const LATEST_PYTHON_3_14: PythonVersion = PythonVersion::new(3, 14, 0);
 
 /// The Python version that was requested for a project.
